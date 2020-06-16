1e0f7645735851a25a2870975adcef87be89d9b6
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-06-15 22:38:26 -0400

    Initally commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const socket = io()
+
+// // Send a message to say that I've connected
+// socket.emit('newuser', {user: 'Grace Hopper'})
+
+// // Event listener, waiting for an incoming "newuser"
+// socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
+
+
+// Listen for the 'submit' of a form
+// 	 event.preventDefault()  (prevent the form from leaving the page)
+//   Emit a message using "chatmsg"
+// Listen for "chatmsg"
+//   add a <li> with the chat msg to the <ol>
+
+const $msgForm = document.getElementById('sendMsg')
+const $msgList = document.getElementById('messages')
+const $userName = document.getElementById('userInput')
+let enteredUserName
+
+$userName.addEventListener('submit',(event) =>{
+	event.preventDefault()
+	enteredUserName = event.currentTarget.userName.value
+	socket.emit('username',{name: event.currentTarget.userName.value})
+	// console.log(event.currentTarget.userName.value)
+})
+
+socket.on('username' , (data) => {
+	console.log(`Hello ${data.name}`)
+	
+})
+
+
+$msgForm.addEventListener('submit', (event) => {
+	event.preventDefault()
+	if(enteredUserName)
+	{
+		socket.emit('chatmsg', {msg: event.currentTarget.txt.value , user: enteredUserName})	
+	}
+	
+	event.currentTarget.txt.value = ''
+})
+
+
+
+
+
+	socket.on('chatmsg', (data) => {
+		if(data.user == enteredUserName)
+		{
+			console.log("giiiii")
+		}
+		console.log(`${data.user} : ${data.msg}`)
+		const newMsg = document.createElement('p')
+		$msgList.appendChild(newMsg)
+		newMsg.style.color = "red"
+		newMsg.style.textAlign="center"
+		newMsg.textContent = data.msg
+	})
