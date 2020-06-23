de3f5a43f768e413582db2f48d24f32a5d06a0ab
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-06-23 00:49:13 -0400

    Updated the Css of the chat box

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const socket = io()
+const socket = io();
 
 // // Send a message to say that I've connected
 // socket.emit('newuser', {user: 'Grace Hopper'})
@@ -18,28 +18,31 @@ const socket = io()
 const $msgForm = document.getElementById('sendMsg')
 const $msgList = document.getElementById('messages')
 const $userName = document.getElementById('userInput')
+const $Name = document.getElementById('name')
+const $UserNameInput = document.getElementById("userName")
 let enteredUserName
 
-$userName.addEventListener('submit',(event) =>{
+$userName.addEventListener('submit', (event) => {
 	event.preventDefault()
 	enteredUserName = event.currentTarget.userName.value
-	socket.emit('username',{name: event.currentTarget.userName.value})
+	socket.emit('username', { name: event.currentTarget.userName.value })
+	if ($UserNameInput.value) {
+		$userName.classList.add("form-chat1")
+	}
 	// console.log(event.currentTarget.userName.value)
 })
 
-socket.on('username' , (data) => {
+socket.on('username', (data) => {
 	console.log(`Hello ${data.name}`)
-	
+	$Name.innerHTML = `By : ${data.name}`
 })
 
 
 $msgForm.addEventListener('submit', (event) => {
 	event.preventDefault()
-	if(enteredUserName)
-	{
-		socket.emit('chatmsg', {msg: event.currentTarget.txt.value , user: enteredUserName})	
+	if (enteredUserName) {
+		socket.emit('chatmsg', { msg: event.currentTarget.txt.value, user: enteredUserName })
 	}
-	
 	event.currentTarget.txt.value = ''
 })
 
@@ -47,17 +50,15 @@ $msgForm.addEventListener('submit', (event) => {
 
 
 
-	socket.on('chatmsg', (data) => {
-		
-	
-		console.log(`${data.user} : ${data.msg}`)
-		const newMsg = document.createElement('p')
-		$msgList.appendChild(newMsg)
-		
-		newMsg.textContent =  data.msg
-		if(data.user == enteredUserName){
-			newMsg.style.textAlign="right"
-			newMsg.style.color = "red"
-			console.log("giiiii")
-		}
-	})
+socket.on('chatmsg', (data) => {
+	console.log(`${data.user} : ${data.msg}`)
+	const newMsg = document.createElement('p')
+	$msgList.appendChild(newMsg)
+
+	newMsg.textContent = data.msg
+	if (data.user == enteredUserName) {
+		newMsg.style.textAlign = "right"
+		newMsg.style.color = "red"
+		console.log("giiiii")
+	}
+})
